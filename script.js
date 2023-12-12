a57f1401239522d6f7c98a9deb1508faf67daace
Luís Teixeira <84951329+LuisT-ls@users.noreply.github.com>
2023-12-12 15:03:21 +0000

    Remodificação no layout

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-// Seleciona o elemento nav fora da função para reutilização
-const navElement = document.querySelector('nav')
-
-// Toggle menu quando clicar no ícone de menu mobile
-document.querySelector('.botao-menu').addEventListener('click', function () {
-  navElement.classList.toggle('ativo')
-})
-
-// Função para alternar o menu
-function toggleMenu() {
-  navElement.classList.toggle('ativo')
+// Função para abrir o menu
+function abrirMenu() {
+  document.querySelector('.nav').style.display = 'block'
 }
+
+// Função para fechar o menu
+function fecharMenu() {
+  document.querySelector('.nav').style.display = 'none'
+}
+
+// Função para chamar a chamada para ação
+function chamarChamadaParaAcao() {
+  alert('Chamou a chamada para ação!')
+}
+
+// Função para visualizar mais informações sobre a pintura mediúnica
+function visualizarMaisInformacoes() {
+  // Obtém o elemento da galeria
+  const galeria = document.querySelector('.galeria')
+
+  // Verifica se a galeria está aberta
+  if (galeria.style.display === 'none') {
+    // Abre a galeria
+    galeria.style.display = 'flex'
+  } else {
+    // Fecha a galeria
+    galeria.style.display = 'none'
+  }
+}
+
+// Eventos
+document.querySelector('.hamburger').addEventListener('click', abrirMenu)
+document.querySelector('.nav .fechar').addEventListener('click', fecharMenu)
+document
+  .querySelector('.chamada-para-acao a')
+  .addEventListener('click', chamarChamadaParaAcao)
+document
+  .querySelector('.galeria')
+  .addEventListener('click', visualizarMaisInformacoes)
